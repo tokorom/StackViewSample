40c35569eb2a792e8a1ba25eb66f1465c3001ce9
tokorom <tokorom@gmail.com>
2022-09-07 16:11:53 +0900

    First commit

---
+++ StackViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewSample
+//
+//  Created by Yuta Tokoro on 2022/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
